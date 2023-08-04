3cf67d7f9d39b39243c081e9798c1adaf62d7a99
asliulusoy <wyneshalite@gmail.com>
2023-08-04 14:34:45 +0300

    code explained

--- question.py
+++ question.py
@@ -7,19 +7,27 @@ import math
 #defining the function
 def longest_substring(str):
            
-    test_string="" #creating an empty string
-    max_length = -1 #creating a integer to hold the values we gathered
+    test_string="" #an empty character string named test_string is created. we'll use this string to store temporary substrings during the process.
+    max_length = -1 #this variable will store the length of the longest unique substring we obtain.
      
-    #finding the lenght of the input string, if it is 0 or 1, then the function will print an error message
+    #finding the lenght of the input string;
     if (len(str) == 0):
-        return 0
+        return 0 #if the length of the string is 0, the function returns 0, indicating an empty string.
     elif(len(str) == 1):
-        return 1
+        return 1 #if the length of the string is 1, the function returns 1, indicating a single character in the string.
+    
+    '''
+     If the special cases above are passed, a loop begins. This loop aims to traverse the characters of the str string one by one to find the longest unique substring.
+
+    While iterating over each character, a temporary substring called temp containing the character i is created.
+    '''
     for i in list(str):
         temp = "".join(i)
         if (temp in test_string):
-            test_string = test_string[test_string.index(temp) + 1:]
-        test_string = test_string + "".join(i)
+            test_string = test_string[test_string.index(temp) + 1:] #now, it's checked whether the temp substring has been seen before in the test_string.
+            #if it's been seen, the test_string is updated starting from the position immediately after the last occurrence of the temp substring. 
+            #this helps us remove previous unique substrings and create a new unique substring.
+        test_string = test_string + "".join(i) #next, the temp substring is added to the test_string, thus updating the test_string to include a new unique substring.
         max_length = max(len(test_string), max_length)
     return test_string
  
