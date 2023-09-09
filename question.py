a2ef833d596005c33b3bc17444de71f99a544d46
aslı <wyneshalitie@gmail.com>
2023-09-09 17:49:06 +0300

    updated

--- question.py
+++ question.py
@@ -1,40 +1,37 @@
-'''
-The purpose of the function named "longestSubstring" is to find the
-longest sub-string in a string that does not contain any duplicate. 
-'''
-import math
+# The purpose of the function named "longest_substring" is to find the
+# longest sub-string in a string that does not contain any duplicates.
 
-#defining the function
-def longest_substring(str):
-           
-    test_string="" #an empty character string named test_string is created. we'll use this string to store temporary substrings during the process.
-     
-    #finding the lenght of the input string;
-    if (len(str) == 0):
-        return 0 #if the length of the string is 0, the function returns 0, indicating an empty string.
-    elif(len(str) == 1):
-        return 1 #if the length of the string is 1, the function returns 1, indicating a single character in the string.
-    
-    '''
-     If the special cases above are passed, a loop begins. This loop aims to traverse the characters of the str string one by one to find the longest unique substring.
+# Importing math is not needed for this code.
 
-    While iterating over each character, a temporary substring called temp containing the character i is created.
-    '''
-    for i in list(str):
-        temp = "".join(i)
-        if (temp in test_string):
-            test_string = test_string[test_string.index(temp) + 1:] #now, it's checked whether the temp substring has been seen before in the test_string.
-            #if it's been seen, the test_string is updated starting from the position immediately after the last occurrence of the temp substring. 
-            #this helps us remove previous unique substrings and create a new unique substring.
-        test_string = test_string + "".join(i) #next, the temp substring is added to the test_string, thus updating the test_string to include a new unique substring.
-    return test_string
- 
+# Defining the function
+def longest_substring(s):
+    longest = ""  # Initialize an empty string to store the longest unique substring.
+    current = ""  # Initialize an empty string to store the current unique substring.
+
+    # Iterate over each character in the input string.
+    for char in s:
+        if char not in current:
+            current += char  # Add the character to the current unique substring.
+        else:
+            if len(current) > len(longest):
+                longest = current  # Update the longest substring if needed.
+            # Find the index of the repeated character in the current unique substring
+            index = current.index(char)
+            # Remove all characters before and including the repeated character.
+            current = current[index + 1:] + char
+
+    # Check one more time after the loop ends to ensure we capture the longest substring.
+    if len(current) > len(longest):
+        longest = current
+
+    return longest
+
+# Function to run test cases
 def run_tests():
     test_cases = [
         ("bbbbb", "b"),
-        #("", ""),
+        ("", ""),
         ("ABBCDDEFGHII", "DEFGHI"),
-        #("AABBCCD", "AB" or "BC" or "CD")
         ("ABCD", "ABCD"),
         ("dvdf", "vdf"),
     ]
@@ -46,9 +43,8 @@ def run_tests():
         else:
             print(f"Test case {idx+1}: FAILED\nExpected: {expected_output}\nActual: {result}")
 
-
 if __name__ == "__main__":
-    input_string = input("Enter a string: ")
+    input_string = input("Enter a string:: ")
     length = len(longest_substring(input_string))
-    print("The length of the longest non-repeating character substring is",length, " and the substring is: ", longest_substring(input_string))
+    print("The length of the longest non-repeating character substring is", length, " and the substring is: ", longest_substring(input_string))
     run_tests()
